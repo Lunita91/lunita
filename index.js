4a89e56f9d897b4a28c104db4b322820ed8035a5
Lunita91 <157089185+Lunita91@users.noreply.github.com>
2024-01-19 19:45:39 -0500

    Update index.js
    Signed-off-by: Lunita91 <157089185+Lunita91@users.noreply.github.com>

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('NANI\nBOT\nMAX', {
+say('Lunita\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
